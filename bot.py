14f7686861bf329fc35268dbcd1bf06b51d4fa24
Voidlaw <43623322+Voidlaw@users.noreply.github.com>
2020-10-31 09:08:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ def agent(obs, config):
                 if ship.cell.halite < 100:
                     neighbors = [ship.cell.north.halite, ship.cell.east.halite, 
                                  ship.cell.south.halite, ship.cell.west.halite]
-                    ### Add an If checking the amount of halite
+                    
                     best = max(range(len(neighbors)), key=neighbors.__getitem__)
                     ship.next_action = directions[best]
                 
@@ -73,7 +73,155 @@ def agent(obs, config):
     return me.next_actions
   
 
+ 
+%%writefile submission2.py
+
+# Imports helper functions
+from kaggle_environments.envs.halite.helpers import *
+
+# Returns best direction to move from one position (fromPos) to another (toPos)
+# Example: If I'm at pos 0 and want to get to pos 55, which direction should I choose?
+def getDirTo(fromPos, toPos, size):
+    fromX, fromY = divmod(fromPos[0],size), divmod(fromPos[1],size)
+    toX, toY = divmod(toPos[0],size), divmod(toPos[1],size)
+    if fromY < toY: return ShipAction.NORTH
+    if fromY > toY: return ShipAction.SOUTH
+    if fromX < toX: return ShipAction.EAST
+    if fromX > toX: return ShipAction.WEST
+
+# Directions a ship can move
+directions = [ShipAction.NORTH, ShipAction.EAST, ShipAction.SOUTH, ShipAction.WEST]
+
+# Will keep track of whether a ship is collecting halite or carrying cargo to a shipyard
+ship_states = {}
+
+# Returns the commands we send to our ships and shipyards
+def agent(obs, config):
+    size = config.size
+    board = Board(obs, config)
+    me = board.current_player
+
+    # If there are no ships, use first shipyard to spawn a ship.
+    if len(me.ships) == 0 and len(me.shipyards) > 0:
+        me.shipyards[0].next_action = ShipyardAction.SPAWN
+
+    # If there are no shipyards, convert first ship into shipyard.
+    if len(me.shipyards) == 0 and len(me.ships) > 0:
+        me.ships[0].next_action = ShipAction.CONVERT
+    
+    for ship in me.ships:
+        if ship.next_action == None:
+            
+            ### Part 1: Set the ship's state 
+            if ship.halite < 200: # If cargo is too low, collect halite
+                ship_states[ship.id] = "COLLECT"
+            if ship.halite > 500: # If cargo gets very big, deposit halite
+                ship_states[ship.id] = "DEPOSIT"
+                
+            ### Part 2: Use the ship's state to select an action
+            if ship_states[ship.id] == "COLLECT":
+                # If halite at current location running low, 
+                # move to the adjacent square containing the most halite
+                if ship.cell.halite < 100:
+
+                    neighbors_halite = [ship.cell.north.halite, ship.cell.east.halite, 
+                                       ship.cell.south.halite, ship.cell.west.halite,
+                                       ship.cell.north.west.halite,ship.cell.north.east.halite,
+                                       ship.cell.south.east.halite,ship.cell.south.west.halite]
+
+                    neighbors = [ship.cell.north.position, ship.cell.east.position, 
+                                       ship.cell.south.position, ship.cell.west.position,
+                                       ship.cell.north.west.position,ship.cell.north.east.position,
+                                       ship.cell.south.east.position,ship.cell.south.west.position]
+                    
+                    best = max(range(len(neighbors_halite)), key=neighbors_halite.__getitem__) 
+                    ship.next_action = getDirTo(ship.position,neighbors[best] , size)
+                    
+                    #print("NN: ",ship.cell.north.north.position,"\n","NE:",ship.cell.north.east.position)
+                
+            if ship_states[ship.id] == "DEPOSIT":
+                # Move towards shipyard to deposit cargo
+                direction = getDirTo(ship.position, me.shipyards[0].position, size)
+                if direction: ship.next_action = direction
+                
+    return me.next_actions
+
+
+%%writefile submission3.py
+
+# Imports helper functions
+from kaggle_environments.envs.halite.helpers import *
+
+# Returns best direction to move from one position (fromPos) to another (toPos)
+# Example: If I'm at pos 0 and want to get to pos 55, which direction should I choose?
+def getDirTo(fromPos, toPos, size):
+    fromX, fromY = divmod(fromPos[0],size), divmod(fromPos[1],size)
+    toX, toY = divmod(toPos[0],size), divmod(toPos[1],size)
+    if fromY < toY: return ShipAction.NORTH
+    if fromY > toY: return ShipAction.SOUTH
+    if fromX < toX: return ShipAction.EAST
+    if fromX > toX: return ShipAction.WEST
+
+# Directions a ship can move
+directions = [ShipAction.NORTH, ShipAction.EAST, ShipAction.SOUTH, ShipAction.WEST]
+
+# Will keep track of whether a ship is collecting halite or carrying cargo to a shipyard
+ship_states = {}
+
+# Returns the commands we send to our ships and shipyards
+def agent(obs, config):
+    size = config.size
+    board = Board(obs, config)
+    me = board.current_player
+
+    # If there are no ships, use first shipyard to spawn a ship.
+    if len(me.ships) == 0 and len(me.shipyards) > 0:
+        me.shipyards[0].next_action = ShipyardAction.SPAWN
+
+    # If there are no shipyards, convert first ship into shipyard.
+    if len(me.shipyards) == 0 and len(me.ships) > 0:
+        me.ships[0].next_action = ShipAction.CONVERT
+    
+    for ship in me.ships:
+        if ship.next_action == None:
+            
+            ### Part 1: Set the ship's state 
+            if ship.halite < 200: # If cargo is too low, collect halite
+                ship_states[ship.id] = "COLLECT"
+            if ship.halite > 1000: # If cargo gets very big, deposit halite
+                ship_states[ship.id] = "DEPOSIT"
+                
+            ### Part 2: Use the ship's state to select an action
+            if ship_states[ship.id] == "COLLECT":
+                # If halite at current location running low, 
+                # move to the adjacent square containing the most halite
+                if ship.cell.halite < 100:
+
+                    neighbors_halite = [ship.cell.north.halite, ship.cell.east.halite, 
+                                       ship.cell.south.halite, ship.cell.west.halite,
+                                       ship.cell.north.west.halite,ship.cell.north.east.halite,
+                                       ship.cell.south.east.halite,ship.cell.south.west.halite]
+
+                    neighbors = [ship.cell.north.position, ship.cell.east.position, 
+                                       ship.cell.south.position, ship.cell.west.position,
+                                       ship.cell.north.west.position,ship.cell.north.east.position,
+                                       ship.cell.south.east.position,ship.cell.south.west.position]
+                    
+                    best = max(range(len(neighbors_halite)), key=neighbors_halite.__getitem__) 
+                    ship.next_action = getDirTo(ship.position,neighbors[best] , size)
+                    
+                    #print("NN: ",ship.cell.north.north.position,"\n","NE:",ship.cell.north.east.position)
+                
+            if ship_states[ship.id] == "DEPOSIT":
+                # Move towards shipyard to deposit cargo
+                direction = getDirTo(ship.position, me.shipyards[0].position, size)
+                if direction: ship.next_action = direction
+                
+    return me.next_actions
+
+
+
 from kaggle_environments import make
 env = make("halite", debug=True)
-env.run(["submission.py", "submission.py", "submission.py", "submission.py"])
+env.run(["submission.py", "submission2.py", "submission3.py", "random"])
 env.render(mode="ipython", width=800, height=600)
